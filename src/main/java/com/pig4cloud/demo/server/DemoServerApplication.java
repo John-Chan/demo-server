ea39362f5eafe8c3f906baa5d82d589796caea55
cj <power4j@outlook.com>
2020-12-21 20:22:32 +0800

    index page

--- src/main/java/com/pig4cloud/demo/server/DemoServerApplication.java
+++ src/main/java/com/pig4cloud/demo/server/DemoServerApplication.java
@@ -2,7 +2,11 @@ package com.pig4cloud.demo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class DemoServerApplication {
 
@@ -10,4 +14,8 @@ public class DemoServerApplication {
 		SpringApplication.run(DemoServerApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public ResponseEntity<String> index(){
+		return ResponseEntity.ok("最小镜像挑战赛<br/>© 2020 pig4clooud.com");
+	}
 }
